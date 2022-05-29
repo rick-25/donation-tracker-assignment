a4751226c9276aded7972398a91c45445b52a8d0
Aditya Kumar <araj7060@gmail.com>
2022-05-29 10:42:58 +0530

    added style to modal

--- src/components/App.js
+++ src/components/App.js
@@ -16,9 +16,20 @@ const App = (props) => {
         profileUrl: "https://yt3.ggpht.com/ytc/AKedOLTYmj1X3VgQCftFxIIxGH5leO14nBVJCctR2r6m1Q=s176-c-k-c0x00ffffff-no-rj-mo",
         profession: "YouTuber"
     };
-    const data = [demodata, demodata, demodata, demodata, demodata, demodata, demodata, demodata];
+    const data = [demodata, demodata, demodata, demodata, demodata, demodata, demodata, {
+        id: "132323",
+        userName: "Aditya Kumar",
+        profileUrl: "https://yt3.ggpht.com/ytc/AKedOLTYmj1X3VgQCftFxIIxGH5leO14nBVJCctR2r6m1Q=s176-c-k-c0x00ffffff-no-rj-mo",
+        profession: "Nalla"
+    }];
 
     const handleClick = (creatorId) => { setActiveId(creatorId) };
+    const getCreator =(creatorId) => {
+        for(let i=0; i<data.length; i++) {
+            if(data[i].id == creatorId) return data[i];
+        }
+        return null;
+    }
 
     return (
         <div className="app">
@@ -31,7 +42,12 @@ const App = (props) => {
                     />
                 )}
             </div>
-            <CreatorModal creator={demodata}/>
+            {
+                (activeId.length > 0) ? 
+                    <CreatorModal creator={getCreator(activeId)} hide={() => {setActiveId("")}}/>
+                : null
+            }
+            
         </div>
     )
 };
--- src/components/CreatorModal.js
+++ src/components/CreatorModal.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+import '../css/creatormodal.css'
 
 
-const CreatorModal = ({ creator }) => {
+const CreatorModal = ({ creator, hide }) => {
+
+    const [amount, setAmount] = useState(0);
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+
     return (
         <div className="creator-modal">
-            <div className="title">{creator.userName}</div>
-            <div className="container">
-                <div className="message">Send your love to {creator.userName} to become a real fan</div>
-            </div>
-            <div className="form">
-                <form>
-                    <div className="amount">
-                        <i>Hello</i>
-                        <input type="text" placeholder="200"/>
+            <div className="modal-frame" onClick={(e) => { hide() }}>
+                <div className="popup" onClick={(e) => e.stopPropagation()}>
+                    <div className="title">{creator.userName}</div>
+                    <div className="container">
+                        <div className="message">Send your love to {creator.userName} to become a real fan</div>
+                        <form onSubmit={(e) => hide()}>
+                            <div className="amount input-border">
+                                <span>$</span>
+                                <input type="number" placeholder="200" onChange={(e) => setAmount(e.target.value)} />
+                            </div>
+                            <input type="text" placeholder="Your name(optional)" className="input-border" onChange={(e) => setName(e.target.value)} />
+                            <textarea placeholder="Say something nice(optional)" className="input-border" onChange={(e) => setMessage(e.target.value)} />
+                            <button type="submit">Support $ {amount}</button>
+                        </form>
                     </div>
-                    <input type="text" placeholder="Your name(optional)"/>
-                    <textarea placeholder=""/>
-                </form>
+                </div>
             </div>
         </div>
-    );    
+    );
 };
 
 export default CreatorModal;
